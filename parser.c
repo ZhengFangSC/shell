8949b9fb4e61aaccd3450b2107fbff8117a26860
Riley Trautman <asonix.dev@gmail.com>
2016-02-08 20:21:58 -0600

    Parses unless newline in command

--- parser.c
+++ parser.c
@@ -58,8 +58,28 @@ void parse_and_exec(char *line) {
   printf("%s\n", line);
 
   char **args = get_arguments(line);
-  printf("%s\n", get_path(args[0]));
 
+  if (args) {
+    if (*(args + 0)) {
+      printf("%s: %s\n", "First elem", *(args + 0));
+
+      char *path = get_path(*(args + 0)); // first element
+
+      if (strcmp(path, "") != 0) {
+        printf("%s\n", path);
+
+        for (unsigned int i = 0; i < 50; ++i) {
+          if (*(args + i)) {
+            free(*(args + i));
+            *(args + i) = NULL;
+          }
+        }
+      }
+    }
+
+    free(args);
+    args = NULL;
+  }
 }
 
 char *get_path(const char *cmd) {
@@ -67,12 +87,16 @@ char *get_path(const char *cmd) {
   // for "ls"
   // will return "/usr/bin/ls"
 
-  char *path;
+  printf("%s: %s\n", "Getting path", cmd);
+
+  char *path = NULL;
   unsigned int i = 0;
 
   while (environ[i]) {
     if (starts_with("PATH=", environ[i])) {
-      path = environ[i] + 5; // start 5 characters into the word
+      path = malloc(strlen(environ[i] + 5) * sizeof(char));
+      strcpy(path, environ[i] + 5); // start 5 characters into the word
+      break;
     }
     i++;
   }
@@ -81,22 +105,33 @@ char *get_path(const char *cmd) {
   char *p = NULL;
 
   for (p = strtok(path, ":"); p; p = strtok(NULL, ":")) {
-    if (dest) {
-      free(dest);
-    }
-
-    dest = (char*) malloc(sizeof(char) *
-                          (strlen(p) + strlen("/") + strlen(cmd)));
+    dest = malloc(sizeof(char) * (strlen(p) + strlen("/") + strlen(cmd) + 1));
     strcpy(dest, p);
     strcat(dest, "/");
     strcat(dest, cmd);
 
     if (access(dest, F_OK) >= 0) {
+      // if (path) { // this errors out when not commented
+      //   free(path);
+      //   path = NULL;
+      // }
       return dest;
     }
+
+    if (dest) {
+      free(dest);
+      dest = NULL;
+    }
   }
+
+  // if (path) { // this errors out when not commented
+  //   free(path);
+  //   path = NULL;
+  // }
+
   // error because command not in PATH
-  return "nope";
+
+  return "";
 }
 
 bool starts_with(const char *start, const char *line) {
@@ -111,8 +146,13 @@ bool starts_with(const char *start, const char *line) {
 }
 
 char **get_arguments(const char *line) {
-  char **tokens = malloc(sizeof(char*) * 50); // accept a maximum of 50 args
+  char **tokens = malloc(50 * sizeof(char*)); // accept a maximum of 50 args
+
   unsigned int token_iterator = 0;
+
+  for (unsigned int i = 0; i < 50; ++i) {
+    tokens[i] = NULL;
+  }
 
   bool new_token = true;
   bool seen_text = false;
@@ -123,13 +163,13 @@ char **get_arguments(const char *line) {
   bool is_double_quote_string = false;
   bool is_single_quote_string = false;
 
-  printf("%s\n", "Initialized things");
-
   for (unsigned int i = 0; i < strlen(line); ++i) {
     printf("%s: %c\n", "Current char", line[i]);
     printf("%s: %s\n", "Current char is spacey", is_spacey(line[i]) ? "True" : "False");
     printf("%s: %s\n", "Seen text", (seen_text ? "True" : "False"));
     printf("%s: %s\n", "New token", (new_token ? "True" : "False"));
+    printf("%s: %s\n", "Double quote", (is_double_quote_string ? "True" : "False"));
+    printf("%s: %s\n", "Single quote", (is_single_quote_string ? "True" : "False"));
 
     if (is_spacey(line[i]) && new_token &&
         !is_single_quote_string && !is_double_quote_string) {
@@ -138,15 +178,23 @@ char **get_arguments(const char *line) {
         new_token = true;
         seen_text = false;
       }
-    } else if (is_spacey(line[i]) && seen_text) {
+    } else if (is_spacey(line[i]) && seen_text &&
+               !is_single_quote_string && !is_double_quote_string) {
       if (i > 0 && line[i-1] == '\\') {
-        new_token = true;
-        seen_text = false;
+        new_token = false;
+        seen_text = true;
       } else {
         printf("chose second\n");
         printf("%s: %i\n", "End value", i);
         end_value = i;
-        tokens[token_iterator] = parse_string(line, start_value, end_value);
+
+        char *tmp = parse_string(line, start_value, end_value);
+        char **tmp_storage = tokens + token_iterator;
+        *tmp_storage = malloc(sizeof(char) * strlen(tmp));
+        strcpy(*tmp_storage, tmp);
+        free(tmp);
+        tmp = NULL;
+
         token_iterator++;
         new_token = true;
       }
@@ -179,9 +227,14 @@ char **get_arguments(const char *line) {
 }
 
 char *parse_string(const char *line, unsigned int start, unsigned int end) {
-  printf("%s\n", "Started parsing string");
   // this needs to be changed to handle strings with $ inside
-  char *out = malloc(end+1 - start);
-  strncpy(out, line + start, end - start);
+
+  int size = (end-start) * sizeof(char);
+
+  char *out = NULL;
+  out = malloc(size + 1);
+  strncpy(out, line + start, end-start);
+  out[end] = '\0';
+
   return out;
 }
